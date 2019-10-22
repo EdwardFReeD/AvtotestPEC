6dcc257fbce076b06755db7800b0d47359800969
grachev.ea <EdwardGra4ev@yahoo.com>
2019-10-22 14:23:00 +0300

    Переделан test_calc и test_line

--- test_calc.py
+++ test_calc.py
@@ -8,86 +8,83 @@ from selenium.webdriver.common.by import By
 import sys
 
 def calc():
- # try:
+ try:
 
     driver = webdriver.Chrome(executable_path='chromedriver.exe')  # chromedriver.exe должен находиться в той же папке, что и этот скрипт!
 
     driver.get("https://pecom.ru/services-are/shipping-request/")  # Перейти по ссылк
     # Удаление адреса
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[1]/a'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[1]/a'))).click()
 
     # Удаление адреса
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/a[2]'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/a[2]'))).click()
     # заполнение A
-    a = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[1]/textarea')))
-    s = 'Россия, Оренбург, Малышевская улица, 28' # Костыль 22.60.2019 посмотрим сколько проработает
+    a = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/div/div[1]/div[1]/textarea')))
+    s = 'Россия, Оренбург, Малышевская улица, 28' # Костыль 22.10.2019 посмотрим сколько проработает
     a.send_keys(s)  # отправляем текст в строку поиска
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/div[1]/div[3]')))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/div[1]/div[3]')))
     a.send_keys(Keys.RETURN)
     #Забор
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/label'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/label'))).click()
     # заполняем B
-    b = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[1]/textarea')))
-    s = 'Россия, Санкт-Петербург, Дворцовая площадь, 6-8'# Костыль 22.60.2019 посмотрим сколько проработает
+    b = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[1]/textarea')))
+    s = 'Россия, Санкт-Петербург, Дворцовая площадь, 6-8'# Костыль 22.10.2019 посмотрим сколько проработает
     b.send_keys(s)
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[3]')))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/div[3]')))
     b.send_keys(Keys.RETURN)  # имитируем нажатие клавиши Enter  bid-btn btn btn-blue
 
      # Проверка на ошибку
-    if WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div[1]/div[1]/div[3]/button'))).text != 'Оформить заявку':
+    if WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div[1]/div[1]/div[3]/button'))).text != 'Оформить заявку':
         zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||' + 'Подача заявки: Не удалось перейти на шаг 2')
     else:
-        WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div[1]/div[1]/div[3]/button'))).text == 'Оформить заявку'
-        WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CLASS_NAME,'bid-btn.btn.btn-blue'))).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/div[1]/div[1]/div[3]/button'))).text == 'Оформить заявку'
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME,'bid-btn.btn.btn-blue'))).click()
 
     # характер гурза
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[1]'))).click()
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[2]/div/div[1]'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[1]'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[2]/div/div[1]'))).click()
 
     # Отправитель
     time.sleep(5)
-    har = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CLASS_NAME, 'bid-agent-new__button')))
+    har = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'bid-agent-new__button')))
     har.click()
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/div/div/div[3]'))).click()
-    phone =  WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[3]/div/div[2]/input')))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/div/div/div[3]'))).click()
+    phone =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[3]/div/div[2]/input')))
     phone.send_keys('1111111111')
-    fam =  WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[5]/div/input')))
+    fam =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[5]/div/input')))
     fam.send_keys('Тест')
-    name =  WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[7]/div/input')))
+    name =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[7]/div/input')))
     name.send_keys('Тест')
 
-    doc = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[60]/div[2]/div/div/div[1]/input')))
+    doc = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[10]/div[2]/div/div/div[1]/input')))
     doc.click()
     doc.send_keys(Keys.RETURN)
 
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[60]/div[3]/div/div[1]/div[2]/div/input'))).send_keys('1111')
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[60]/div[3]/div/div[2]/div[2]/div/input'))).send_keys('111111')
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/button'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[10]/div[3]/div/div[1]/div[2]/div/input'))).send_keys('1111')
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div/form/div[10]/div[3]/div/div[2]/div[2]/div/input'))).send_keys('111111')
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[2]/div[1]/button'))).click()
 
     # получатель
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[3]/div[2]/div/div[1]/div[2]/div/div[2]/div'))).click()
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[3]/div[2]/div/div[2]/div[1]/button'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[3]/div[2]/div/div[1]/div[2]/div/div[2]/div/div[2]'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[3]/div[2]/div/div[2]/div[1]/button'))).click()
 
     # Плательщик
-    com = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[4]/div[2]/div/div[2]/div[5]/div/textarea')))
+    com = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[4]/div[2]/div/div[2]/div[5]/div/textarea')))
     com.click()
     com.send_keys('PECOM_MONITORING')
 
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[4]/div[2]/div/div[3]/label/span'))).click()
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[4]/div[2]/div/div[4]/div[1]/button[1]'))).click()
-    zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'+ 'Подача заявки:Заявка подана успешно, номер зявки ' + WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[1]/div[2]'))).text)
- # except Exception:
- #        if sys.exc_info() != None:
- #            if 'element click intercepted' in sys.exc_info()[1].msg:
- #                zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'
- #                           + 'Ошибка TimeOut страницы: '
- #                           + sys.exc_info()[1].msg)
- #            else:
- #                zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'
- #                               + 'Необработанная ошибка: '
- #                               + sys.exc_info()[1].msg)
- #        else:
- #            zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'
- #                           + 'Ошибка TimeOut страницы: ')
- # finally:
- #        driver.close()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[4]/div[2]/div/div[3]/label/span'))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[1]/div/div[4]/div[2]/div/div[4]/div[1]/button[1]'))).click()
+    zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'+ 'Подача заявки:Заявка подана успешно, номер зявки ' + WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="page"]/div[2]/div/div/div/div/div/div[1]/div/div[3]/div[1]/div[2]/div[2]/div[1]/div[2]'))).text)
+ except Exception:
+        if sys.exc_info() != None:
+            if 'element click intercepted' in sys.exc_info()[1].msg:
+                zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'
+                           + 'Ошибка TimeOut страницы: '
+                           + sys.exc_info()[1].msg).replace('\n', ' ')
+            else:
+                zapis.log_read('||' + "https://pecom.ru/services-are/shipping-request/" + '||'
+                               + 'Необработанная ошибка: '
+                               + sys.exc_info()[1].msg).replace('\n', ' ')
+ finally:
+        driver.close()
--- test_line.py
+++ test_line.py
@@ -1,24 +1,25 @@
 import time
 import zapis
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import sys
 def test_line():
         driver = webdriver.Chrome(executable_path = 'chromedriver.exe')  # chromedriver.exe должен находиться в той же папке, что и этот скрипт!
         driver.get("https://pecom.ru/contacts/feedback/")  # Перейти по ссылк
-        select = driver.find_element_by_class_name('selectize-input.items.not-full.has-options').click() # нажимаем на класс
-        time.sleep(1)
-        select1 = driver.find_element_by_class_name('option.active').click()# еще один клик
-        time.sleep(1)
-        control = driver.find_element_by_id('form_text_37') # выбираем
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME,'selectize-input.items.not-full.has-options'))).click() # нажимаем на класс
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME,'option.active'))).click()# еще один клик
+
+        control = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'form_text_37')))# выбираем
         control.send_keys("МСВК02ДГЯ-1/1610")  # отправляем текст в строку поиска
         control.send_keys(Keys.RETURN)  # имитируем нажатие клавиши Enter
-        time.sleep(10) # спати
-        driver.find_element_by_id('form_text_38').click()# клик
-        time.sleep(1)
-        er = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div/div/div[1]/form/div[2]/div/div/div[3]/span').text.replace('\n', ' ')# замена
-        time.sleep(1)
+
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'form_text_38'))).click()# клик
+
+        er =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div[2]/div[2]/div[1]/div/div/div/div/div[1]/form/div[2]/div/div/div[3]/span'))).text.replace('\n', ' ')# замена
+
         if er == 'Груз не найден. Без указания груза вы не сможете отправить запрос.':# если равно то ошибка
            driver.close()
            zapis.log_read('||' + "https://pecom.ru/contacts/feedback/" + '||' + 'Ошибка: Груз не найден или поиск не отработал за 20 секунд.')
